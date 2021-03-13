707b44b5cc66c54c48c9c1ad48f719e5169d20f0
Brendan Hall <30509293+blhall195@users.noreply.github.com>
2021-03-13 17:52:04 +0000

    included new sleep libary

--- cave_logger_arduino_code/cave_logger_arduino_code.ino
+++ cave_logger_arduino_code/cave_logger_arduino_code.ino
@@ -12,8 +12,6 @@ double pressure_abs, depth, pressure_baseline;
 //------------------------------------------------------
 
 
-
-
 //RTC code----------------------------------------------
 #include <Wire.h>
 #include "RTClib.h" //libary containing real time clock code
@@ -41,8 +39,13 @@ int flag; //for signaling module power on/off to make device more efficent when 
 int sensorpowerpin = 10; //pin for powering ulta sonic sensor
 int ledpowerpin = 7; //pin for powering display
 int rtcpowerpin = 9; //pin for powering all the peripheral devices sensors ect, but not the memory card slot
-#include <Adafruit_SleepyDog.h>//import sleep code
-int sleep_time;
+
+#include <Sleep_n0m1.h>
+int count = 0;
+Sleep sleep;
+unsigned long sleepTime_1; //how long you want the arduino to sleep
+unsigned long sleepTime_2; //how long you want the arduino to sleep
+
 //---------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -61,8 +64,15 @@ void setup() {
   pinMode(ledpowerpin, OUTPUT);
   pinMode(rtcpowerpin, OUTPUT);
   digitalWrite(ledpowerpin, HIGH);//turn on display power
-  digitalWrite(rtcpowerpin, HIGH);//turn on display power
+  digitalWrite(rtcpowerpin, LOW);//turn on display power
   delay(100);
+
+
+  //Define Sleep code
+  sleepTime_1 = 1000; //set sleep time in ms, max sleep time is 49.7 days
+  //sleepTime_2 = 1000; //set sleep time in ms, max sleep time is 49.7 days
+  sleepTime_2 = 3000; //set sleep time in ms (5min), max sleep time is 49.7 days
+
 
   // Define pressure sensor code
   Wire.begin();
@@ -74,7 +84,8 @@ void setup() {
 
   //------------------------------
   //RTC
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //to set clock time to time of compile uncomment this line
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //to set clock time to time of compile uncomment this line
+  //rtc.disable32K();
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
@@ -82,34 +93,34 @@ void setup() {
   //------------------------------------------------------------------------
 
 
-//Display code------------------------------------------------------------
+  //Display code------------------------------------------------------------
 #if RST_PIN >= 0
-oled.begin(&Adafruit128x32, I2C_ADDRESS, RST_PIN);
+  oled.begin(&Adafruit128x32, I2C_ADDRESS, RST_PIN);
 #else // RST_PIN >= 0
   oled.begin(&Adafruit128x32, I2C_ADDRESS);
 #endif // RST_PIN >= 0
-// Call oled.setI2cClock(frequency) to change from the default frequency.
- oled.setFont(System5x7);
-//------------------------------------------------------------------------
+  // Call oled.setI2cClock(frequency) to change from the default frequency.
+  oled.setFont(System5x7);
+  //------------------------------------------------------------------------
 
 
   //  //SD card code------------------------------------------------------------
-    Serial.print("Initializing SD card...");
-oled.println("Initializing SD card...");
-    delay(500);
-  
-    if (!SD.begin(4)) {
-      Serial.println("initialization failed!");
-  oled.println("initialization failed!");
-      while (1);
-    }
-    Serial.println("initialization done.");
-oled.println("initialization done.");
-    delay(1000);
-//  //  //-----------------------------------------------------------------------
-//
-//  //flag signals when to clear and turn off the oled display,
-//  //increase this number if you want the display to be on longer after the device is reset
+  Serial.print("Initializing SD card...");
+  oled.println("Initializing SD card...");
+  delay(500);
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    oled.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  oled.println("initialization done.");
+  delay(1000);
+  //  //  //-----------------------------------------------------------------------
+  //
+  //  //flag signals when to clear and turn off the oled display,
+  //  //increase this number if you want the display to be on longer after the device is reset
   flag = 10;
   //-------------------------------------------------------------------------------------------------------------------------------
 }
@@ -122,32 +133,34 @@ oled.println("initialization done.");
 
 void loop() {
   while (flag > 1) {
+    check_battery();
     flag = flag - 1;
     Serial.print(flag); //prints the flag number over serial for debugging
     detect_distance(); //detect distance
     display_oled(); //request time from RTC and print time/distance to display
     cavedataLog(); //request time from RTC and save to time/distance to SD card
-    digitalWrite(rtcpowerpin, LOW); //turn off rtc power before going to sleep
-    sleep_time = 1000;
-    int sleepMS = Watchdog.sleep(sleep_time); //sleep for x milliseconds
-    digitalWrite(rtcpowerpin, HIGH); //turn on rtc power
     digitalWrite(ledpowerpin, HIGH); //turns off the display power pin
     delay(100);
     digitalWrite(ledpowerpin, LOW); //turns off the display power pin
+
+    sleep.pwrDownMode(); //set sleep mode
+    sleep.sleepDelay(sleepTime_1); //sleep for: sleepTime
+
     if (flag < 2) {
       oled.clear(); //clear the OLED display
     }
   }
-  digitalWrite(rtcpowerpin, HIGH);  //turn off rtc power before going to sleep
+
+  check_battery();
   detect_distance(); //detect distance
   cavedataLog();  //request time from RTC and save to time/distance to SD card
+  sleep.pwrDownMode(); //set sleep mode
+  sleep.sleepDelay(sleepTime_2); //sleep for: sleepTime
+  Serial.print("tewst"); //prints the flag number over serial for debugging
   digitalWrite(ledpowerpin, HIGH); //turns off the display power pin
   delay(100);
   digitalWrite(ledpowerpin, LOW); //turns off the display power pin
-  digitalWrite(rtcpowerpin, LOW);  //turn off rtc power before going to sleep
-  sleep_time = 1000;
-  int sleepMS = Watchdog.sleep(sleep_time); //sleep for x milliseconds
-  //digitalWrite(rtcpowerpin, HIGH);  //turn on rtc power
+
 }
 
 
@@ -166,25 +179,64 @@ void cavedataLog(void) {
   // if the file opened okay, write to it:
   if (CavedataLog) {
     Serial.print("Writing to test.txt...");
-    CavedataLog.print(now.day(), DEC);
-    CavedataLog.print('/');
-    CavedataLog.print(now.month(), DEC);
-    CavedataLog.print('/');
+
     CavedataLog.print(now.year(), DEC);
+    CavedataLog.print('/');
+
+    if (now.month() < 10) {
+      CavedataLog.print("0");
+    }
+    CavedataLog.print(now.month(), DEC);
+
+    CavedataLog.print('/');
+
+    if (now.day() < 10) {
+      CavedataLog.print("0");
+    }
+    CavedataLog.print(now.day(), DEC);
+
     CavedataLog.print(" ");
+
+    if (now.hour() < 10) {
+      CavedataLog.print("0");
+    }
     CavedataLog.print(now.hour(), DEC);
+
     CavedataLog.print(':');
+
+    if (now.minute() < 10) {
+      CavedataLog.print("0");
+    }
     CavedataLog.print(now.minute(), DEC);
+
     CavedataLog.print(':');
+
+    if (now.second() < 10) {
+      CavedataLog.print("0");
+    }
     CavedataLog.print(now.second(), DEC);
+
     CavedataLog.print(", ");
     CavedataLog.print(depth);
     CavedataLog.print(", ");
-    CavedataLog.print(temperature_c);  
+    CavedataLog.print(temperature_c);
+
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+    float voltage = sensorValue * (6.6 / 1023.0);
+    // print out the value you read:
+    CavedataLog.print(", ");
+    CavedataLog.print(voltage);
     CavedataLog.println("");
+    Serial.println(" ");
+    Serial.print("Voltage: ");
+    Serial.print(voltage);
+
+
     // close the file:
     CavedataLog.close();
-    Serial.println("done.");
+    Serial.println(" ");
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
@@ -195,9 +247,11 @@ void cavedataLog(void) {
 //------------------------------------------------------------------------------------------------------------
 
 
+
+
 //function for triggering sensor and returning distance-------------------------------------------------------
 void detect_distance(void) {
-    // Read temperature from the sensor in deg C. This operation takes about
+  // Read temperature from the sensor in deg C. This operation takes about
   temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
 
   // Read pressure from the sensor in mbar.
@@ -216,15 +270,41 @@ void detect_distance(void) {
 
   Serial.print("Depth (cm) = ");
   Serial.println(depth);
+  Serial.println("done.");
 
   Serial.println(" ");//padding between outputs
+}
+//------------------------------------------------------------------------------------------------------------
 
+
+
+
+//function for shutting down the device if the battery gets too low-------------------------------------------------------
+void check_battery(void) {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage = sensorValue * (6.6 / 1023.0);
   // print out the value you read:
-  Serial.println(voltage);
+  if (voltage < 4.5) {
+    oled.clear(); //clear the OLED display
+    oled.println("Battery low");
+    oled.println();
+    oled.println("shutting down");
+    CavedataLog.print("Battery low shutting down");
+    Serial.println("Battery low shutting down");//padding between outputs
+    delay(500);
+    while (voltage < 10) {
+      Serial.println("sleeping");
+      Serial.println("sleeping");
+      Serial.println("sleeping");
+      sleep.pwrDownMode(); //set sleep mode
+
+
+    }
+  }
+  Serial.println("Battery okay");//padding between outputs
+  oled.clear(); //clear the OLED display
 }
 //------------------------------------------------------------------------------------------------------------
 
@@ -235,23 +315,48 @@ void display_oled(void) {
   DateTime now = rtc.now();
   oled.clear();
   oled.set1X();
+
   oled.print(now.year(), DEC);
   oled.print('/');
+
+  if (now.month() < 10) {
+    oled.print('0');
+  }
   oled.print(now.month(), DEC);
+
   oled.print('/');
+
+  if (now.day() < 10) {
+    oled.print('0');
+  }
   oled.print(now.day(), DEC);
+
   oled.print(" ");
+
+  if (now.hour() < 10) {
+    oled.print('0');
+  }
   oled.print(now.hour(), DEC);
+
   oled.print(':');
+
+  if (now.minute() < 10) {
+    oled.print('0');
+  }
   oled.print(now.minute(), DEC);
+
   oled.print(':');
+
+  if (now.second() < 10) {
+    oled.print('0');
+  }
   oled.print(now.second(), DEC);
+
   oled.println();
   Serial.print("Temperature: ");
   Serial.print(rtc.getTemperature());
   Serial.println(" C");
   oled.println();
-  Serial.print("Temperature: ");
   //------------------------------
 
   //------------------------------
